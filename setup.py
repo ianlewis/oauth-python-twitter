97a2966ef1b4fd9ace4dbd95ab01f1f83b591916
Sean Brant <brant.sean@gmail.com>
2010-01-11 20:05:21 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oauth-python-twitter',
+    version='0.2',
+    description='A derivative of twitter.Api class to allow Oauth to be used for authentication.',
+    author='Hameedullah Khan',
+    author_email='hameed@hameedkhan.net',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
